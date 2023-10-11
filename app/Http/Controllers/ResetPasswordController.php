360167f27c5261214a6b7a3b02bf98877ddc4950
damjanos-kanzurov <kandamjan@gmail.com>
2023-10-11 16:13:00 +0200

    implement password reset

---
+++ app/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+class ResetPasswordController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email'
+        ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return response()->json(['status' => __($status)], 201);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        $responseCode = $status === Password::PASSWORD_RESET ? 201 : 422;
+
+        return response()->json(['status' => __($status)], $responseCode);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -21,6 +22,9 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/forgot-password', [ResetPasswordController::class, 'store'])->name('password.forgot');
+Route::put('/reset-password', [ResetPasswordController::class, 'update'])->name('password.reset');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
   Route::get('/dashboard', function () {
